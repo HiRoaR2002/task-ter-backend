06e3085524dff4b3c86e98d58509dae07953077c
HiRoaR2002 <himanshgtam2002@gmail.com>
2023-07-12 15:55:39 +0530

    back

--- Auth/route.js
+++ Auth/route.js
@@ -2,6 +2,14 @@ const express = require("express")
 const router = express.Router()
 const { register, login, logout, profile, tasks, removetask } = require("./auth");
 const { protect } = require("../middleware/auth");
+
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+});
 router.route("/register").post(register)
 router.route("/login").post(login);
 router.route("/profile").get(protect, profile);
